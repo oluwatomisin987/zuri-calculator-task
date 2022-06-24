b3928edcf81da1a4316894c7e022dc84166feccd
OLUWATOMISIN <hardex.tommy@gmail.com>
2022-06-24 17:30:29 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var opr = prompt("Operator: ")
+var num1 = parseInt(prompt("Number 1: "))
+var num2 = parseInt(prompt("Number 2: "))
+
+
+
+if(opr== "+")
+{  
+    alert(num1 + num2)
+} else if (opr == "-")
+{
+  alert(num1 - num2)
+} else if (opr == "*")
+{
+  alert(num1 * num2)
+} else if (opr == "/")
+{
+  alert(num1 / num2)
+  
+}
+else{
+  alert ("Enter a valid operator")
+}
+
+alert(results)
